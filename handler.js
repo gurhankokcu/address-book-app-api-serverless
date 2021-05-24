1178f3efbd4f0b9a089b45293f4b4e6df81f19fd
Gurhan Kokcu <gurhankokcu@gmail.com>
2021-05-24 20:09:36 +0100

    add eslint for coding style

--- handler.js
+++ handler.js
@@ -1,15 +1,19 @@
-"use strict";
+'use strict'
 
-module.exports.hello = async (event) => {
+async function hello (event) {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v2.0! Your function executed successfully!",
-        input: event,
+        message: 'Go Serverless v2.0! Your function executed successfully!',
+        input: event
       },
       null,
       2
-    ),
-  };
-};
+    )
+  }
+}
+
+module.exports = {
+  hello
+}
